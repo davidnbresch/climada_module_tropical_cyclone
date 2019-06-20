648a5750f3cfe2556243587be81f9492a94f22c7
davidnbresch <david.bresch@gmail.com>
2019-06-20 14:21:43 +0200

    all in one

--- code/climada_tc_damage.m
+++ code/climada_tc_damage.m
@@ -21,6 +21,9 @@
 %global climada_global
 if ~climada_init_vars,return;end % init/import global variables
 
+% PARAMETERS
+markersize=1;
+
 tc_track=climada_tc_read_unisys_track;
 entity=climada_entity_load;
 hazard=climada_tc_hazard_set(tc_track,'NOSAVE',entity);
@@ -41,3 +44,20 @@ params.unit_scale=1e6;params.cbar_ylabel='damage';
 climada_entity_plot(entity_dmg,1,params);hold on;
 title_str=sprintf('%s, damage %3.2g USD',tc_track.name,EDS.ED);
 title(title_str);box on
+
+hazard_ts=climada_ts_hazard_set(hazard,'NOSAVE');
+EDS(2)=climada_EDS_calc(entity,hazard_ts);
+
+figure('Name','storm surge');
+%climada_entity_plot(entity_1km,markersize);hold on;
+climada_hazard_plot(hazard_ts,-1,markersize);hold on;
+plot(tc_track.lon,tc_track.lat,'-k');hold on;
+entity_dmg=entity; % make a copy to plot damage
+entity_dmg.assets.Value=EDS(2).ED_at_centroid';
+entity_dmg.assets.Value(entity_dmg.assets.Value<=1)=NaN;
+params.unit_scale=1e6;params.cbar_ylabel='damage';
+climada_entity_plot(entity_dmg,markersize,params);hold on;
+title_str=sprintf('%s, surge damage %3.2g USD',tc_track.name,EDS(2).ED);
+title(title_str);box on
+
+fprintf('combined damage: %3.2g USD\n',EDS(1).ED+EDS(2).ED);
